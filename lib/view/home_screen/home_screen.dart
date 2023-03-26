666cb9c7ceb11c4888758d0d7ca5c3c0234085c5
Anton Iankin <kerjen01@gmail.com>
2023-03-26 04:03:58 +0300

    [FEATURE] Added a pull to refresh feature & updated a bottom nav bar

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -20,8 +20,6 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           Container(),
           Container(),
-          Container(),
-          Container(),
           const ProfileScreen(),
         ],
       ),
@@ -34,36 +32,32 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         destinations: [
           const NavigationDestination(
-            icon: Icon(Icons.home_outlined),
-            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.work_outline),
+            selectedIcon: Icon(Icons.work),
             label: 'Home',
           ),
           const NavigationDestination(
-            icon: Icon(Icons.search_outlined),
-            selectedIcon: Icon(Icons.search),
-            label: 'Search',
+            icon: Icon(Icons.list_alt_outlined),
+            selectedIcon: Icon(Icons.list_alt),
+            label: 'Replies',
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              height: 40,
-              width: 72,
-              margin: const EdgeInsets.only(left: 8, right: 8, top: 12),
-              decoration: BoxDecoration(
-                color: lightColorScheme.primaryContainer,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Icon(
-                Icons.add,
-                color: lightColorScheme.primary,
-              ),
-            ),
-          ),
-          const NavigationDestination(
-            icon: Icon(Icons.chat_outlined),
-            selectedIcon: Icon(Icons.chat),
-            label: 'Chat',
-          ),
+          // Align(
+          //   alignment: Alignment.topCenter,
+          //   child: Container(
+          //     height: 40,
+          //     width: 72,
+          //     margin: const EdgeInsets.only(left: 8, right: 8, top: 12),
+          //     decoration: BoxDecoration(
+          //       color: lightColorScheme.primaryContainer,
+          //       borderRadius: BorderRadius.circular(20),
+          //     ),
+          //     child: Icon(
+          //       Icons.add,
+          //       color: lightColorScheme.primary,
+          //     ),
+          //   ),
+          // ),
+
           const NavigationDestination(
             icon: Icon(Icons.person_outline),
             selectedIcon: Icon(Icons.person),
--- lib/view/profile_screen/general_screen/general_screen.dart
+++ lib/view/profile_screen/general_screen/general_screen.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import '../../../core/ui/color_schemes.dart';
 import '../../common/country_dropdown/country_dropdown.dart';
 import '../../common/loading_indicator.dart';
@@ -16,75 +16,88 @@ class ProfileGeneralScreen extends StatefulWidget {
 }
 
 class _ProfileGeneralScreenState extends State<ProfileGeneralScreen> {
+  final _refreshController = RefreshController(initialRefresh: false);
   final _emailTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ProfileBloc, ProfileState>(
-      listener: (context, state) {
-        state.maybeMap(
-          profile: (state) {
-            _emailTextController.text = state.email;
-          },
-          orElse: () {},
-        );
+    return SmartRefresher(
+      controller: _refreshController,
+      onRefresh: () {
+        context.read<ProfileBloc>().add(const ProfileEvent.update());
       },
-      builder: (context, state) {
-        return state.maybeMap(
-          profile: (state) {
-            return SingleChildScrollView(
-              padding: const EdgeInsets.only(top: 32, left: 24, right: 24),
-              child: Column(
-                children: [
-                  _buildProfileInfo(
-                    photo: state.photo,
-                    firstName: state.firstName,
-                    lastName: state.lastName,
-                    bio: state.bio,
-                  ),
-                  const SizedBox(height: 8),
-                  const Divider(color: dividerColor),
-                  const SizedBox(height: 24),
-                  TextField(
-                    controller: _emailTextController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.email_outlined),
-                      labelText: 'Email',
-                      hintText: 'example@mail.com',
+      header: WaterDropMaterialHeader(
+        color: lightColorScheme.primary,
+        backgroundColor: lightColorScheme.primaryContainer,
+        distance: 30,
+      ),
+      child: BlocConsumer<ProfileBloc, ProfileState>(
+        listener: (context, state) {
+          state.maybeMap(
+            profile: (state) {
+              _refreshController.refreshCompleted();
+              _emailTextController.text = state.email;
+            },
+            orElse: () {},
+          );
+        },
+        builder: (context, state) {
+          return state.maybeMap(
+            profile: (state) {
+              return SingleChildScrollView(
+                padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+                child: Column(
+                  children: [
+                    _buildProfileInfo(
+                      photo: state.photo,
+                      firstName: state.firstName,
+                      lastName: state.lastName,
+                      bio: state.bio,
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  // TextField(
-                  //   keyboardType: TextInputType.phone,
-                  //   inputFormatters: [
-                  //     MaskTextInputFormatter(
-                  //       mask: '+# (###) ###-##-##',
-                  //       filter: {'#': RegExp(r'[0-9]')},
-                  //     ),
-                  //   ],
-                  //   decoration: const InputDecoration(
-                  //     prefixIcon: Icon(Icons.phone_outlined),
-                  //     labelText: 'Phone',
-                  //     hintText: '+1 (234) 567-89-00',
-                  //   ),
-                  // ),
-                  // const SizedBox(height: 16),
-                  CountryDropdown(
-                    initialCountryId: state.countryId,
-                    onChoosed: (entry) {},
-                  )
-                ],
-              ),
-            );
-          },
-          orElse: () {
-            return const Center(
-              child: AppLoadingIndicator(),
-            );
-          },
-        );
-      },
+                    const SizedBox(height: 8),
+                    const Divider(color: dividerColor),
+                    const SizedBox(height: 24),
+                    TextField(
+                      controller: _emailTextController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.email_outlined),
+                        labelText: 'Email',
+                        hintText: 'example@mail.com',
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    // TextField(
+                    //   keyboardType: TextInputType.phone,
+                    //   inputFormatters: [
+                    //     MaskTextInputFormatter(
+                    //       mask: '+# (###) ###-##-##',
+                    //       filter: {'#': RegExp(r'[0-9]')},
+                    //     ),
+                    //   ],
+                    //   decoration: const InputDecoration(
+                    //     prefixIcon: Icon(Icons.phone_outlined),
+                    //     labelText: 'Phone',
+                    //     hintText: '+1 (234) 567-89-00',
+                    //   ),
+                    // ),
+                    // const SizedBox(height: 16),
+                    CountryDropdown(
+                      initialCountryId: state.countryId,
+                      onChoosed: (entry) {},
+                    )
+                  ],
+                ),
+              );
+            },
+            orElse: () {
+              return const Center(
+                child: AppLoadingIndicator(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 
